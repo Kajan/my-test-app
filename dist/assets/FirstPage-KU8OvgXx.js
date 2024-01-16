3dc8bedd531350e846179b510e294b47dfed28ef
Kajaneshan Senthilnathan <uk_senthilk@us.fctg.travel>
2024-01-16 13:51:01 +0000

    adding dist

---
+++ dist/assets/FirstPage-KU8OvgXx.js
@@ -0,0 +1,1 @@
+import{d as o,r as n,c as a,a as s,t as r,o as c}from"./index-Wn9mJx3m.js";const i={class:"card"},l=s("h5",null," This is from first page. ",-1),f=o({__name:"FirstPage",setup(u){const t=n(0);return(p,e)=>(c(),a("div",i,[s("button",{type:"button",onClick:e[0]||(e[0]=_=>t.value++)},"count is "+r(t.value),1),l]))}});export{f as default};
