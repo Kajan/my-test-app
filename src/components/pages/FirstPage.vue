7cab21219eb595ab9265f60aab79499138e52297
Kajaneshan Senthilnathan <uk_senthilk@us.fctg.travel>
2024-01-16 15:52:32 +0000

    added okta integration

--- src/components/pages/FirstPage.vue
+++ src/components/pages/FirstPage.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { AuthState } from "@okta/okta-auth-js";
+import { useAuth } from "@okta/okta-vue";
+import { ShallowRef, inject, ref,  } from "vue";
     const count = ref(0)
+    const authState = inject<ShallowRef<AuthState>>('okta.authState');
+    console.log('authState')
+
+    const $auth = useAuth();
+
+    const logout = async () => {
+      await $auth.signOut();
+    } 
 </script>
 
 <template>
@@ -8,6 +18,10 @@ import { ref } from "vue";
     <button type="button" @click="count++">count is {{ count }}</button>
     <h5>
       This is from first page. 
+      User Name is {{ authState?.idToken?.claims.name }}
     </h5>
+    <Button @click="logout()" type="text">Logout</Button>
   </div>
+
+  
 </template>
--- src/components/router/index.ts
+++ src/components/router/index.ts
@@ -1,11 +1,19 @@
+import { LoginCallback, navigationGuard } from "@okta/okta-vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
         path: '/my-test-app/',
         component: () => import("../pages/FirstPage.vue"),
-        name: 'First Page'
-    }
+        name: 'First Page',
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/my-test-app/login/callback',
+        component: LoginCallback
+      }
 ]
 
 
@@ -13,3 +21,5 @@ export const router = createRouter({
     history: createWebHistory('/'),
     routes,
 })
+
+router.beforeEach(navigationGuard);
