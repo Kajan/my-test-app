18969ceddc8e936920009677563cb08d75e6e5df
Kajaneshan Senthilnathan <uk_senthilk@us.fctg.travel>
2024-01-18 15:35:38 +0000

    changed the base uri

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/my-test-app/',
-  // base: process.env.NODE_ENV === 'production' 
-  //   ? 'https://kajan.github.io/'
-  //   : '/',
+  //base: '/my-test-app/',
+  base: process.env.NODE_ENV === 'production' 
+    ? 'https://kajan.github.io/my-test-app/'
+    : '/my-test-app/',
 })
